2d10f19ef6e337191bd7504725f583b610e298e3
Riya Fartyal <riyaf3105@gmail.com>
2024-05-07 18:12:52 +0900

    Challenge completed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+  # list all articles
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      # show the form with the restaurant that didnt save (not a brand new one)
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+
+  private
+  def article_params
+    # we are whitelisting the attributes a user can give us
+    params.require(:article).permit(:title, :content)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# requring faker gem
+require 'faker'
+
+# Create 10 fake articles
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 3).join("\n")
+  )
+end
